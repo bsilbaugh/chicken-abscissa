5f9aa8a8062629d02e0f0e6b34a4e2c24fba82ea
Benjamin Silbaugh <bss@mandelbrot.local>
2014-01-07 01:02:02 -0500

    Implemented meta-* functions
    The meta-* functions were introduced as a kind of "meta language" to
    provide a mechanism to configure the appearance of graphical
    elements, output file names, etc.

--- abscissa.scm
+++ abscissa.scm
@@ -9,17 +9,26 @@
 ;;; See LICENSE file for modification and redistribution permissions.
 
 (module abscissa 
-  (window 
+  (meta-window
+   meta-pdf-file
+   meta-cartesian-axes
+   meta-lines
+   meta-points
+   meta-lines-points
+   window 
    pdf-file
    figure
-   axes
+   cartesian-axes
    lines
-   linespoints
-   xy-pairs)
+   points
+   lines-points
+   data-pairs)
 
 (import scheme chicken)
 (use data-structures)
 (use posix)
+
+;;; === helpers ===
 
 (define (interactive win)
   (call-with-output-pipe "gnuplot -persist" win))
@@ -27,55 +36,210 @@
 (define (batch file)
   (call-with-output-pipe "gnuplot" file))
 
-(define (window fig)
+;;; === "Meta" elements ===
+
+(define ((meta-window) fig)
   (interactive
    (lambda (p) (fig p))))
 
-(define (pdf-file fig)
+(define ((meta-pdf-file #!key 
+						(width 5.0) 
+						(height 3.0) 
+						(file-name "abscissa-plot.pdf")) fig)
+  (define (display-size p)
+	(display "size " p)
+	(display width p)
+	(display ", " p)
+	(display height p)
+	(display " " p))
+  (define (display-file-name p)
+	(display #\" p)
+	(display file-name p)
+	(display #\" p)
+	(display " " p))
   (batch
    (lambda (p)
-	 (display "set term pdf" p)
+	 (display "set term pdf " p)
+	 (display-size p)
 	 (newline p)
-	 (display "set output \"test.pdf\" " p)
+	 (display "set output " p)
+	 (display-file-name p)
 	 (newline p)
 	 (fig p))))
 
-(define (figure ax)
+(define ((meta-figure) ax)
   (lambda (p) (ax p)))
 
-(define (axes . cases)
+(define ((meta-cartesian-axes #!key 
+							  (x-label #f)
+							  (y-label #f)
+							  (major-grid #f)) . cases)
   (define (display-comma p)
 	(display ", " p))
+  (define (display-label cmd-str label p)
+	(if label
+		(begin
+		  (display cmd-str p)
+		  (display #\" p)
+		  (display label p)
+		  (display #\" p)
+		  (newline p))))
+  (define (display-x-label p)
+	(display-label "set xlabel " x-label p))
+  (define (display-y-label p)
+	(display-label "set ylabel " y-label p))
+  (define (display-grid p)
+	(cond ((eq? '-- major-grid)
+		   (display "set grid linestyle 0" p))
+		  ((eq? '- major-grid)
+		   (display "set grid linestyle -1" p))
+		  ((fixnum? major-grid)
+		   (display "set grid linestyle " p)
+		   (display (number->string major-grid) p)))
+	(newline p))
   (let ((with-stmts (intersperse (map car cases) display-comma))
 		(data-stmts (map cdr cases)))
 	(lambda (p)
-	  (display "set grid" p)
-	  (newline p)
-	  (display "set xlabel \"X\"" p)
-	  (newline p)
-	  (display "set ylabel \"Y\"" p)
-	  (newline p)
+	  (display-grid p)
+	  (display-x-label p)
+	  (display-y-label p)
 	  (display "plot " p)
 	  (for-each (lambda (f) (f p)) with-stmts)
 	  (newline p)
 	  (for-each (lambda (f) (f p)) data-stmts))))
 
-(define (lines data-set)
+; TODO
+; (define ((meta-polar-axes) . cases))
+
+(define ((meta-lines #!key 
+					 (style '-) 
+					 (color "dark-blue") 
+					 (weight 1)) data-set)
+  (define (line-style s)
+	(cond ((eq? '-  style) -1) ; solid line
+		  ((eq? '-- style) 0) ; dashed line
+		  ((and (fixnum? style) (< 0 style)) style)
+		  (error "Unrecognized line style value: " style)))
+  (define (display-style p)
+	(display "linestyle " p)
+	(display (line-style style) p)
+	(display #\space p))
+  (define (display-color p)
+	(display "lc rgb " p)
+	(display #\" p)
+	(display color p)
+	(display #\" p)
+	(display #\space p))
+  (define (display-weight p)
+	(display "lw " p)
+	(display weight p)
+	(display " " p))
   (define (with-stmt p)
-	(display "'-' with lines" p))
+	(display "'-' with lines " p)
+	(display-style p)
+	(display-color p)
+	(display-weight p))
   (cons with-stmt data-set))
 
-(define (points data-set)
+(define ((meta-points #!key 
+					  (style 'o) 
+					  (color "dark-blue") 
+					  (weight 1)) data-set)
+  (define (line-type style)
+	(cond ((eq? 'o style) 32)
+		  ((eq? '+ style) 27)
+		  ((eq? 'x style) 28)
+		  ((eq? 's style) 30)
+		  ((eq? 'd style) 25)
+		  ((eq? 't style) 34)))
+  (define (display-style p)
+	(display "linetype " p)
+	(display (line-type style) p)
+	(display #\space p))
+  (define (display-color p)
+	(display "lc rgb " p)
+	(display #\" p)
+	(display color p)
+	(display #\" p)
+	(display #\space p))
+  (define (display-weight p)
+	(display "ps " p)
+	(display weight p)
+	(display #\space p))
   (define (with-stmt p)
-	(display "'-' with points" p))
+	(display "'-' with points " p)
+	(display-style p)
+	(display-color p)
+	(display-weight p))
   (cons with-stmt data-set))
 
-(define (linespoints data-set)
+(define ((meta-lines-points #!key 
+							(style 'o-)
+							(color "dark-blue") 
+							(weight 1)) data-set)
+  (define (line-type style)
+	(cond ((or (eq? 'o- style)
+			   (eq? '+- style)
+			   (eq? 'x- style)
+			   (eq? 's- style)
+			   (eq? 'd- style)
+			   (eq? 't- style)) -1)
+		  ((or (eq? 'o-- style)
+			   (eq? '+-- style)
+			   (eq? 'x-- style)
+			   (eq? 's-- style)
+			   (eq? 'd-- style)
+			   (eq? 't-- style)) 0)))
+  (define (point-type style)
+	(cond ((or (eq? 'o- style) (eq? 'o-- style)) 32)
+		  ((or (eq? '+- style) (eq? '+-- style)) 27)
+		  ((or (eq? 'x- style) (eq? 'x-- style)) 28)
+		  ((or (eq? 's- style) (eq? 's-- style)) 30)
+		  ((or (eq? 'd- style) (eq? 'd-- style)) 25)
+		  ((or (eq? 't- style) (eq? 't-- style)) 34)))
+  (define (display-lines-type p)
+	(display "linetype " p)
+	(display (line-type style) p)
+	(display #\space p))
+  (define (display-point-type p)
+	(display "pointtype " p)
+	(display (point-type style) p)
+	(display #\space p))
+  (define (display-color p)
+	(display "lc rgb " p)
+	(display #\" p)
+	(display color p)
+	(display #\" p))
+  (define (display-weight p)
+	(display "lw " p)
+	(display weight p)
+	(display #\space p))
   (define (with-stmt p)
-	(display "'-' with linespoints" p))
+	(display "'-' with linespoints " p)
+	(display-lines-type p)
+	(display-point-type p)
+	(display-color p)
+	(display-weight p))
   (cons with-stmt data-set))
 
-(define (xy-pairs pairs)
+
+;;; === Plot Elements ===
+
+(define window (meta-window))
+
+(define pdf-file (meta-pdf-file))
+
+(define figure (meta-figure))
+
+(define cartesian-axes (meta-cartesian-axes))
+
+(define lines (meta-lines))
+
+(define points (meta-points))
+
+(define lines-points (meta-lines-points))
+
+(define (data-pairs pairs)
   (lambda (p)
 	(letrec ((display-pair
 			  (lambda (xy)
